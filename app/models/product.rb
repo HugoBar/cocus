520e366986df7155e305ead805d511a1bc65a315
HugoBar <hugomdbarreiro@gmail.com>
2025-10-23 16:08:02 +0100

    Fix base units

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   has_many :recipe_products
   has_many :recipes, through: :recipe_products
 
-  validates :unit, presence: true, inclusion: { in: %w[pcs g ml tsp tbsp cup] }
+  validates :unit, presence: true, inclusion: { in: %w[pcs g ml] }
 end
