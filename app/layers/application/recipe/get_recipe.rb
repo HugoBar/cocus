2cb1cdb64110b9a9d9379551cdbc0baa56476139
HugoBar <hugomdbarreiro@gmail.com>
2026-02-12 14:24:03 +0000

    Add application service to get recipes

--- app/layers/application/recipe/get_recipe.rb
+++ app/layers/application/recipe/get_recipe.rb
@@ -1,12 +1,34 @@
 module Application
   module Recipe
+    # Application use case responsible for retrieving a recipe
+    # along with its associated products.
+    #
+    # It coordinates domain repositories but does not contain
+    # business rules itself.
     class GetRecipe
-      def initialize(recipe_repository:)
+      # Initializes the use case with required dependencies.
+      #
+      # @param recipe_repository [Domains::Recipe::RecipeRepository]
+      # @param product_repository [Domains::Product::ProductRepository]
+      def initialize(recipe_repository:, product_repository:)
         @recipe_repository = recipe_repository
+        @product_repository = product_repository
       end
 
+      # Executes the use case.
+      #
+      # Fetches the recipe by ID and loads the products referenced
+      # by the recipe's ingredients.
+      #
+      # @param id [Integer] the recipe ID
+      # @return [Hash] containing:
+      #   - :recipe [Domains::Recipe::Recipe]
+      #   - :products [Array<Domains::Product::Product>]
       def call(id:)
-        @recipe_repository.find(id)
+        recipe = @recipe_repository.find(id)
+        products = @product_repository.find_by_ids(recipe.ingredients.map(&:product_id))
+
+        { recipe: recipe, products: products }
       end
     end
   end
